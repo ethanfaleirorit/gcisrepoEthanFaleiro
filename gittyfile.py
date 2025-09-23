d13b8534773332c4e07fec163f2e6b2a2aa3172d
ethanfaleirorit <ef9008@g.rit.edu>
2025-09-23 10:35:43 +0400

    Adding file

---
+++ gittyfile.py
@@ -0,0 +1,9 @@
+number1 = int(input("Please enter a number: "))
+if number1 % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
+sqnumber1 = number1**2
+cbnumber1 = number1**3
+print("The square is",sqnumber1)
+print("The cube is",cbnumber1)
