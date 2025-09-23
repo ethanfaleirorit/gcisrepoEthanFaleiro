8260e13faaecab4f98e944b0c85f6b16ad443e1c
ethanfaleirorit <ef9008@g.rit.edu>
2025-09-23 10:45:05 +0400

    sending

--- gittyfile.py
+++ gittyfile.py
@@ -1,9 +1,20 @@
-number1 = int(input("Please enter a number: "))
-if number1 % 2 == 0:
-    print("The number is even")
-else:
-    print("The number is odd")
-sqnumber1 = number1**2
-cbnumber1 = number1**3
-print("The square is",sqnumber1)
-print("The cube is",cbnumber1)
+def input_num():
+    global number1
+    number1 = int(input("Please enter a number: "))
+    
+    ''' This function allows the user to input the required number.'''
+def evenodd():
+    if number1 % 2 == 0: #Checks if the number is even or odd
+        print("The number is even") #Confirms the number is even
+    else:
+        print("The number is odd") #Confirms the number is odd
+def expo():
+    sqnumber1 = number1**2 #Calculates the exponential 2
+    cbnumber1 = number1**3 #Calculates the exponential 3        
+    print("The square is",sqnumber1)
+    print("The cube is",cbnumber1)
+def main():
+    input_num()
+    evenodd()
+    expo()
+main()
